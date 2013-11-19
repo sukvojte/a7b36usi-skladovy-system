7060556ec631b074841cb7e977af9f340c813b82
sourepet <sourepet@fel.cvut.cz>
2013-11-19 18:40:48 +0100

    conf

--- source/src/main/java/cz/a7b36usi/sklad/App.java
+++ source/src/main/java/cz/a7b36usi/sklad/App.java
@@ -1,9 +1,7 @@
 package cz.a7b36usi.sklad;
 
-import cz.a7b36usi.sklad.BO.Test;
 import cz.a7b36usi.sklad.BO.Tester;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
