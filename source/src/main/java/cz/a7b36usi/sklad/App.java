8c5e2be3972d871204b73aee2cad21215887b68b
Vojtech Suk <vojta@nemilkov.cz>
2013-11-20 00:36:01 +0100

    Merge branch 'gui2' of github.com:usi-skladovy-system/a7b36usi-skladovy-system into gui2
    Conflicts:
    	source/src/main/java/cz/a7b36usi/sklad/App.java
    	source/src/main/java/cz/a7b36usi/sklad/Controller/LoginController.java
    	source/src/main/java/cz/a7b36usi/sklad/Controller/MainController.java

--- source/src/main/java/cz/a7b36usi/sklad/App.java
+++ source/src/main/java/cz/a7b36usi/sklad/App.java
@@ -1,18 +1,9 @@
 package cz.a7b36usi.sklad;
 
-import cz.a7b36usi.sklad.BO.Test;
-import cz.a7b36usi.sklad.BO.Tester;
-<<<<<<< HEAD
-import cz.a7b36usi.sklad.Controller.LoginController;
-import cz.a7b36usi.sklad.gui.Prihlaseni;
-
-=======
-import cz.a7b36usi.sklad.Controller.ILoginController;
-import cz.a7b36usi.sklad.Controller.LoginController;
->>>>>>> 819b6fbc00aeafacf90cd397be0d059bd461db1e
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cz.a7b36usi.sklad.Controller.LoginController;
 
 
 /**
@@ -31,8 +22,7 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("/applicationContext.xml");
-        
-<<<<<<< HEAD
+
         LoginController login = ctx.getBean(LoginController.class);
         
         login.showLogForm("login");
@@ -41,10 +31,6 @@ public class App
         //t.print();
         
         //new Prihlaseni().setVisible(true);
-=======
-        LoginController l = ctx.getBean(LoginController.class);
-        l.showLogForm(null);
-        
->>>>>>> 819b6fbc00aeafacf90cd397be0d059bd461db1e
+
     }
 }
--- source/src/main/java/cz/a7b36usi/sklad/Controller/LoginController.java
+++ source/src/main/java/cz/a7b36usi/sklad/Controller/LoginController.java
@@ -4,20 +4,11 @@
  */
 package cz.a7b36usi.sklad.Controller;
 
-<<<<<<< HEAD
-=======
-import cz.a7b36usi.gui.IPrihlaseni;
-import cz.a7b36usi.gui.Prihlaseni;
->>>>>>> 819b6fbc00aeafacf90cd397be0d059bd461db1e
-import cz.a7b36usi.sklad.Service.IUserService;
-import cz.a7b36usi.sklad.gui.IPrihlaseni;
-import cz.a7b36usi.sklad.gui.Prihlaseni;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import cz.a7b36usi.sklad.Service.IUserService;
+import cz.a7b36usi.sklad.gui.IPrihlaseni;
 
 /**
  *
@@ -36,14 +27,12 @@ public class LoginController implements ILoginController {
     private IMainController mainController;
     
     public void showLogForm(String message) {
-        System.out.println(message);
-        p.showj(true);
-        //TODO zobrazit ze se login vymrdal 
+        p.setVisible(true);
     }
 
     public void doLogin(String username, String password) {
         if (userService.logInUser(username, password)) {
-            p.showj(false);
+            p.setVisible(false);
             mainController.showMainFrame("Login successfull");
         } else {
             showLogForm("Login unsuccessfull");
