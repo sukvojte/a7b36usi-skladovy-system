dcdd2f90d0f7761a38990ad056a1a63e9a137379
Vojtech Suk <vojta@nemilkov.cz>
2013-11-19 13:57:19 +0100

    Initial commit

---
+++ source/src/main/java/cz/a7b36usi/sklad/BO/ITest.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.a7b36usi.sklad.BO;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Peter
+ */
+@Component
+public interface ITest {
+    public void print();
+}
