62fb0486ebdda02ec07aae464a8ae9802448e89b
sourepet <sourepet@fel.cvut.cz>
2014-01-07 21:03:48 +0100

    getAllWrappingTypesByProduct(Long productId) a getAllProductVersionsByProduct(Long productId) v product service

--- source/src/main/java/cz/a7b36usi/sklad/Service/IProductService.java
+++ source/src/main/java/cz/a7b36usi/sklad/Service/IProductService.java
@@ -10,109 +10,120 @@ import cz.a7b36usi.sklad.DTO.ProductVersionDTO;
 import cz.a7b36usi.sklad.DTO.WrappingTypeDTO;
 
 /**
- * 
+ *
  * @author Lukas L.
  */
 public interface IProductService {
 
-	/**
-	 * Persists product to database. If id is null, then new product will be
-	 * created in database
-	 * 
-	 * @param product
-	 * @return
-	 */
-	public Long saveProduct(ProductDTO product);
+    /**
+     * Persists product to database. If id is null, then new product will be
+     * created in database
+     *
+     * @param product
+     * @return
+     */
+    public Long saveProduct(ProductDTO product);
 
-	/**
-	 * Removes product from database
-	 * 
-	 * @param produktId
-	 *            identifier of product to be removed
-	 */
-	public void deleteProduct(Long produktId);
+    /**
+     * Removes product from database
+     *
+     * @param produktId identifier of product to be removed
+     */
+    public void deleteProduct(Long produktId);
 
-	/**
-	 * Persists category into database. If identifier is null, then new category
-	 * will be created. Note: parrent category is set to null no matter what is
-	 * in DTO param, so tree structure is not implemented yet
-	 * 
-	 * @param category
-	 * @return
-	 */
-	public Long saveCategory(CategoryDTO category);
+    /**
+     * Persists category into database. If identifier is null, then new category
+     * will be created. Note: parrent category is set to null no matter what is
+     * in DTO param, so tree structure is not implemented yet
+     *
+     * @param category
+     * @return
+     */
+    public Long saveCategory(CategoryDTO category);
 
-	/**
-	 * Removes category from database
-	 * 
-	 * @param categoryId
-	 *            identifier of category to be removed
-	 */
-	public void removeCategory(Long categoryId);
+    /**
+     * Removes category from database
+     *
+     * @param categoryId identifier of category to be removed
+     */
+    public void removeCategory(Long categoryId);
 
-	/**
-	 * 
-	 * @return all products from database
-	 */
-	@Transactional(readOnly = true)
-	public List<ProductDTO> getAllProducts();
+    /**
+     *
+     * @return all products from database
+     */
+    @Transactional(readOnly = true)
+    public List<ProductDTO> getAllProducts();
 
-	/**
-	 * 
-	 * @return all categories from database
-	 */
-	@Transactional(readOnly = true)
-	public List<CategoryDTO> getAllCategories();
+    /**
+     *
+     * @return all categories from database
+     */
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> getAllCategories();
 
-	/**
-	 * 
-	 * Persist wrapping type into database. If id is null, then new wrapping
-	 * type will be created
-	 * 
-	 * @param wrap
-	 *            wrapping type to be persisted
-	 * @return identifier of persisted wrapping type
-	 */
-	public Long saveWrappingType(WrappingTypeDTO wrap);
+    /**
+     *
+     * Persist wrapping type into database. If id is null, then new wrapping
+     * type will be created
+     *
+     * @param wrap wrapping type to be persisted
+     * @return identifier of persisted wrapping type
+     */
+    public Long saveWrappingType(WrappingTypeDTO wrap);
 
-	/**
-	 * removes wrapping type from database
-	 * 
-	 * @param id
-	 *            identifier of wrapping type to be removed
-	 */
-	public void removeWrappingType(Long id);
+    /**
+     * removes wrapping type from database
+     *
+     * @param id identifier of wrapping type to be removed
+     */
+    public void removeWrappingType(Long id);
 
-	/**
-	 * 
-	 * @return all wrapping types
-	 */
-	@Transactional(readOnly = true)
-	public List<WrappingTypeDTO> getAllWrappingTypes();
+    /**
+     *
+     * @return all wrapping types
+     */
+    @Transactional(readOnly = true)
+    public List<WrappingTypeDTO> getAllWrappingTypes();
 
-	/**
-	 * 
-	 * Persist version into database. If id is null, then new version will be
-	 * created
-	 * 
-	 * @param version
-	 *            version to be persisted
-	 * @return identifier of persisted version
-	 */
-	public Long saveProductVersion(ProductVersionDTO version);
+    /**
+     *
+     * @param productId identifier of product
+     * @return all wrapping types of one product
+     */
+    @Transactional(readOnly = true)
+    public List<WrappingTypeDTO> getAllWrappingTypesByProduct(Long productId);
 
-	/**
-	 * removes version from database
-	 * 
-	 * @param id
-	 *            identifier of version to be removed
-	 */
-	public void removeProductVersion(Long id);
+    /**
+     *
+     * Persist version into database. If id is null, then new version will be
+     * created
+     *
+     * @param version version to be persisted
+     * @return identifier of persisted version
+     */
+    public Long saveProductVersion(ProductVersionDTO version);
 
-	/**
-	 * 
-	 * @return all product versions
-	 */
-	public List<ProductVersionDTO> getAllProductVersions();
+    /**
+     * removes version from database
+     *
+     * @param id identifier of version to be removed
+     */
+    public void removeProductVersion(Long id);
+
+    /**
+     *
+     * @return all product versions
+     */
+    public List<ProductVersionDTO> getAllProductVersions();
+    
+        /**
+     *
+     * @param productId identifier of product
+     * @return all product versions of one product
+     */
+    @Transactional(readOnly = true)
+    public List<ProductVersionDTO> getAllProductVersionsByProduct(Long productId);
+
 
 }
--- source/src/main/java/cz/a7b36usi/sklad/Service/ProductService.java
+++ source/src/main/java/cz/a7b36usi/sklad/Service/ProductService.java
@@ -15,110 +15,134 @@ import cz.a7b36usi.sklad.DTO.ProductVersionDTO;
 import cz.a7b36usi.sklad.DTO.WrappingTypeDTO;
 
 /**
- * 
+ *
  * @author Lukas L.
  */
 @Component
 public class ProductService extends AbstractService implements IProductService {
 
-	public Long saveProduct(ProductDTO product) {
-		ProductBO bo = new ProductBO();
-		bo.setCategory(product.getCategory() != null ? genericDAO.loadById(
-				product.getCategory(), CategoryBO.class) : null);
-		bo.setCode(product.getCode());
-		bo.setName(product.getName());
-		bo.setQuantity(product.getQuantity());
-		bo.setId(product.getId());
-		return genericDAO.saveOrUpdate(bo).getId();
-	}
+    public Long saveProduct(ProductDTO product) {
+        ProductBO bo = new ProductBO();
+        bo.setCategory(product.getCategory() != null ? genericDAO.loadById(
+                product.getCategory(), CategoryBO.class) : null);
+        bo.setCode(product.getCode());
+        bo.setName(product.getName());
+        bo.setQuantity(product.getQuantity());
+        bo.setId(product.getId());
+        return genericDAO.saveOrUpdate(bo).getId();
+    }
 
-	public void deleteProduct(Long productId) {
-		genericDAO.removeById(productId, ProductBO.class);
-	}
+    public void deleteProduct(Long productId) {
+        genericDAO.removeById(productId, ProductBO.class);
+    }
 
-	public List<ProductDTO> getAllProducts() {
-		List<ProductDTO> dtos = new ArrayList<ProductDTO>();
-		List<ProductBO> bos = genericDAO.getAll(ProductBO.class);
-		for (ProductBO bo : bos) {
-			ProductDTO dto = new ProductDTO(bo.getId(), bo.getName(),
-					bo.getCode(), bo.getQuantity(),
-					(bo.getCategory() != null ? bo.getCategory().getId() : 0));
-			dtos.add(dto);
-		}
-		return dtos;
-	}
+    public List<ProductDTO> getAllProducts() {
+        List<ProductDTO> dtos = new ArrayList<ProductDTO>();
+        List<ProductBO> bos = genericDAO.getAll(ProductBO.class);
+        for (ProductBO bo : bos) {
+            ProductDTO dto = new ProductDTO(bo.getId(), bo.getName(),
+                    bo.getCode(), bo.getQuantity(),
+                    (bo.getCategory() != null ? bo.getCategory().getId() : 0));
+            dtos.add(dto);
+        }
+        return dtos;
+    }
 
-	public Long saveCategory(CategoryDTO category) {
-		CategoryBO bo = new CategoryBO();
-		bo.setId(category.getId());
-		bo.setName(category.getName());
+    public Long saveCategory(CategoryDTO category) {
+        CategoryBO bo = new CategoryBO();
+        bo.setId(category.getId());
+        bo.setName(category.getName());
 
-		bo.setParrent(category.getParrent() != null ? genericDAO.getById(
-				category.getParrent(), CategoryBO.class) : null);
-		return genericDAO.saveOrUpdate(bo).getId();
-	}
+        bo.setParrent(category.getParrent() != null ? genericDAO.getById(
+                category.getParrent(), CategoryBO.class) : null);
+        return genericDAO.saveOrUpdate(bo).getId();
+    }
 
-	public void removeCategory(Long categoryId) {
-		genericDAO.removeById(categoryId, CategoryBO.class);
-	}
+    public void removeCategory(Long categoryId) {
+        genericDAO.removeById(categoryId, CategoryBO.class);
+    }
 
-	public List<CategoryDTO> getAllCategories() {
-		List<CategoryDTO> dtos = new ArrayList<CategoryDTO>();
-		List<CategoryBO> bos = genericDAO.getAll(CategoryBO.class);
-		for (CategoryBO bo : bos) {
-			CategoryDTO dto = new CategoryDTO(bo.getId(), bo.getName(),
-					(bo.getParrent() != null ? bo.getParrent().getId() : null));
-			dtos.add(dto);
-		}
-		return dtos;
-	}
+    public List<CategoryDTO> getAllCategories() {
+        List<CategoryDTO> dtos = new ArrayList<CategoryDTO>();
+        List<CategoryBO> bos = genericDAO.getAll(CategoryBO.class);
+        for (CategoryBO bo : bos) {
+            CategoryDTO dto = new CategoryDTO(bo.getId(), bo.getName(),
+                    (bo.getParrent() != null ? bo.getParrent().getId() : null));
+            dtos.add(dto);
+        }
+        return dtos;
+    }
 
-	public Long saveWrappingType(WrappingTypeDTO wrap) {
-		WrappingTypeBO bo = new WrappingTypeBO();
-		bo.setId(wrap.getId());
-		bo.setName(wrap.getName());
-		bo.setQuantity(wrap.getQuantity());
-		bo.setProduct(genericDAO.loadById(wrap.getProduct(), ProductBO.class));
-		return genericDAO.saveOrUpdate(bo).getId();
-	}
+    public Long saveWrappingType(WrappingTypeDTO wrap) {
+        WrappingTypeBO bo = new WrappingTypeBO();
+        bo.setId(wrap.getId());
+        bo.setName(wrap.getName());
+        bo.setQuantity(wrap.getQuantity());
+        bo.setProduct(genericDAO.loadById(wrap.getProduct(), ProductBO.class));
+        return genericDAO.saveOrUpdate(bo).getId();
+    }
 
-	public void removeWrappingType(Long id) {
-		genericDAO.removeById(id, WrappingTypeBO.class);
-	}
+    public void removeWrappingType(Long id) {
+        genericDAO.removeById(id, WrappingTypeBO.class);
+    }
 
-	public List<WrappingTypeDTO> getAllWrappingTypes() {
-		List<WrappingTypeDTO> dtos = new ArrayList<WrappingTypeDTO>();
-		List<WrappingTypeBO> bos = genericDAO.getAll(WrappingTypeBO.class);
-		for (WrappingTypeBO bo : bos) {
-			WrappingTypeDTO dto = new WrappingTypeDTO(bo.getId(), bo.getName(),
-					bo.getQuantity(), bo.getProduct().getId());
-			dtos.add(dto);
-		}
-		return dtos;
-	}
+    public List<WrappingTypeDTO> getAllWrappingTypes() {
+        List<WrappingTypeDTO> dtos = new ArrayList<WrappingTypeDTO>();
+        List<WrappingTypeBO> bos = genericDAO.getAll(WrappingTypeBO.class);
+        for (WrappingTypeBO bo : bos) {
+            WrappingTypeDTO dto = new WrappingTypeDTO(bo.getId(), bo.getName(),
+                    bo.getQuantity(), bo.getProduct().getId());
+            dtos.add(dto);
+        }
+        return dtos;
+    }
 
-	public Long saveProductVersion(ProductVersionDTO version) {
-		ProductVersionBO bo = new ProductVersionBO();
-		bo.setCode(version.getCode());
-		bo.setName(version.getName());
-		bo.setId(version.getId());
-		bo.setProduct(genericDAO.loadById(version.getProduct(), ProductBO.class));
-		return genericDAO.saveOrUpdate(bo).getId();
-	}
+    public Long saveProductVersion(ProductVersionDTO version) {
+        ProductVersionBO bo = new ProductVersionBO();
+        bo.setCode(version.getCode());
+        bo.setName(version.getName());
+        bo.setId(version.getId());
+        bo.setProduct(genericDAO.loadById(version.getProduct(), ProductBO.class));
+        return genericDAO.saveOrUpdate(bo).getId();
+    }
 
-	public void removeProductVersion(Long id) {
-		genericDAO.removeById(id, ProductVersionBO.class);
-	}
+    public void removeProductVersion(Long id) {
+        genericDAO.removeById(id, ProductVersionBO.class);
+    }
 
-	public List<ProductVersionDTO> getAllProductVersions() {
-		List<ProductVersionDTO> dtos = new ArrayList<ProductVersionDTO>();
-		List<ProductVersionBO> bos = genericDAO.getAll(ProductVersionBO.class);
-		for (ProductVersionBO bo : bos) {
-			ProductVersionDTO dto = new ProductVersionDTO(bo.getId(),
-					bo.getCode(), bo.getName(), bo.getProduct().getId());
-			dtos.add(dto);
-		}
-		return dtos;
-	}
+    public List<ProductVersionDTO> getAllProductVersions() {
+        List<ProductVersionDTO> dtos = new ArrayList<ProductVersionDTO>();
+        List<ProductVersionBO> bos = genericDAO.getAll(ProductVersionBO.class);
+        for (ProductVersionBO bo : bos) {
+            ProductVersionDTO dto = new ProductVersionDTO(bo.getId(),
+                    bo.getCode(), bo.getName(), bo.getProduct().getId());
+            dtos.add(dto);
+        }
+        return dtos;
+    }
+
+    public List<WrappingTypeDTO> getAllWrappingTypesByProduct(Long productId) {
+        List<WrappingTypeDTO> dtos = new ArrayList<WrappingTypeDTO>();
+        List<WrappingTypeBO> bos = genericDAO.getByProperty("product",
+                genericDAO.loadById(productId, ProductBO.class), WrappingTypeBO.class);
+        for (WrappingTypeBO bo : bos) {
+            WrappingTypeDTO dto = new WrappingTypeDTO(bo.getId(), bo.getName(),
+                    bo.getQuantity(), bo.getProduct().getId());
+            dtos.add(dto);
+        }
+        return dtos;
+    }
+
+    public List<ProductVersionDTO> getAllProductVersionsByProduct(Long productId) {
+        List<ProductVersionDTO> dtos = new ArrayList<ProductVersionDTO>();
+        List<ProductVersionBO> bos = genericDAO.getByProperty("product",
+                genericDAO.loadById(productId, ProductBO.class), ProductVersionBO.class);
+        for (ProductVersionBO bo : bos) {
+            ProductVersionDTO dto = new ProductVersionDTO(bo.getId(),
+                    bo.getCode(), bo.getName(), bo.getProduct().getId());
+            dtos.add(dto);
+        }
+        return dtos;
+    }
 
 }
